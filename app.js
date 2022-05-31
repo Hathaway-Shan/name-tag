2af776ea9335b6b9b31072ac2b1eb03256f63c5b
Hathaway-Shan <hathaway.shan@gmail.com>
2022-05-31 16:41:30 -0700

    added pronoun html and css

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // grab DOM elements
 const nameInput = document.getElementById('name-input');
+// get info from user input
 const nameDisplay = document.getElementById('name-display')
 nameInput.addEventListener('input', () => {
     const name = nameInput.value;
+    // use user input to update state
     nameDisplay.textContent = name;
-    
+    // update DOM to reflect the new state
 });
 // set event listeners
-    // get info from user input
-    // use user input to update state
-    // update DOM to reflect the new state
+    
+    

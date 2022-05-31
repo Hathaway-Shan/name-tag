1e67feae6b2b5c336c0b36233aa30642e66091b4
Hathaway-Shan <hathaway.shan@gmail.com>
2022-05-31 16:06:22 -0700

    second variable nameDisplay

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // grab DOM elements
 const nameInput = document.getElementById('name-input');
-console.log('name-input');
-
+const nameDisplay = document.getElementById('name-display')
+console.log(nameDisplay);
 // set event listeners
     // get info from user input
     // use user input to update state

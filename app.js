eaaae9ac3242a56cbdb333fbd9a228dccede3046
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-03 16:54:52 -0700

    checked event listener

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 const nameInput = document.getElementById('name-input');
 // get info from user input
 console.log(nameInput)
+
 const nameDisplay = document.getElementById('name-display');
 nameInput.addEventListener('input', () => {
+    console.log(nameInput.value)
+
     const name = nameInput.value;
     // use user input to update state
     nameDisplay.textContent = name;
+    
     // update DOM to reflect the new state
 });
 // set event listeners
